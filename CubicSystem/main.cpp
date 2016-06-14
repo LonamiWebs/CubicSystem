4484f5ad8787903f8f56392bacaec358d0a31fbb
Lonami <totufals@hotmail.com>
2016-06-14 13:14:39 +0200

    Now draws a rectangle!

--- CubicSystem/main.cpp
+++ CubicSystem/main.cpp
@@ -100,14 +100,21 @@ int main()
 
 	// Set up vertex data and buffers
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f, // Left
-		 0.5f, -0.5f, 0.0f, // Right
-		 0.0f,  0.5f, 0.0f  // Top
+		 0.5f,  0.5f, 0.0f, // Top right
+		 0.5f, -0.5f, 0.0f, // Bottom right
+		-0.5f, -0.5f, 0.0f, // Bottom left
+		-0.5f,  0.5f, 0.0f, // Top left
+	};
+	// Triangles indices
+	GLuint indices[] = {
+		0, 1, 3,
+		1, 2, 3
 	};
 
-	GLuint VBO, VAO;
-	glGenBuffers(1, &VBO);
+	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// Bind our Vertex Array Object first, then bind and set the vertex buffers and attribute pointers
 	glBindVertexArray(VAO);
@@ -115,11 +122,15 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// How to interpret vertex data
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	// Unbind VAO for safety reasons
+	// Unbind for safety reasons
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
 	// Enter the game loop
@@ -136,7 +147,7 @@ int main()
 		// Draw our objects
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		// Swap buffers
@@ -146,6 +157,7 @@ int main()
 	// Clean up resources
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
